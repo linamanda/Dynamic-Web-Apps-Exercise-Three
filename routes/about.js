f338a54b6155fdc3972a35b21b68185b1a7d3428
Amanda Lin <amanda.lin.aut@gmail.com>
2021-10-21 08:38:36 -0400

    Added to /about and /about/me content

--- routes/about.js
+++ routes/about.js
@@ -5,12 +5,15 @@ router.get("/", (reg, res) => {
   res.send(`
     <h1>About Page</h1>
     <p>This is the about page</p>
+    <p><a href="/about/me">Learn about me</a></p>
   `);
 });
 
 router.get("/me", (reg, res) => {
   res.send(`
     <h1>About Me</h1>
+    <p>Information about me...</p>
+    <p><a href="/about">Go back to about page</a></p>
   `);
 });
 
